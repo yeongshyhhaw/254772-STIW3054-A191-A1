7d87034d1bb684fb6797ae03cca8648daabc1322
yeongshyhhaw <47937370+yeongshyhhaw@users.noreply.github.com>
2019-10-24 19:36:55 +0800

    Update Read.java

--- src/main/java/com/company/Read.java
+++ src/main/java/com/company/Read.java
@@ -1,23 +1,21 @@
 package com.company;
 
-
-import org.apache.poi.ss.formula.functions.MatrixFunction;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 import org.jsoup.nodes.Element;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 class Read {
-    private final ArrayList<Data> result = new ArrayList<Data>();
-    public ArrayList<Data>  ReadData() throws IOException {
-        try {
 
-            System.out.println("");
+    int x=0;
+    private final ArrayList<Data> result = new ArrayList<Data>();
+    public void ReadData() throws IOException {
+
+            System.out.println("\n");
             String URL = "https://github.com/STIW3054-A191/Main-Issues/issues/1";
             Document doc = Jsoup.connect(URL).get();
             String tittle = doc.title();
@@ -25,10 +23,6 @@ class Read {
             System.out.println("-----------------------------------------------------------------------------------------");
             Elements trs = doc.getElementsByClass("d-block comment-body markdown-body  js-comment-body" );
             org.jsoup.select.Elements rows = trs.select("ol");
-
-
-
-
             String url="";
             String name = "";
             String matric = "";
@@ -42,6 +36,7 @@ class Read {
                 while(m.find()){
                     name = m.group(0).replace("<br>","").replace("name :","").replace("Name :","").replace("Name:","").replace(":","").replace("Name ","").trim();
                 }
+
 
 
                 /*find student's github link*/
@@ -59,16 +54,23 @@ class Read {
                 /*add name,matric and githublink inside arraylist*/
                 result.add(new Data(name,matric,url));
             }
+            System.out.println("| No. | Matric | Name                                  | GitHub Link                            |");
+            System.out.println("|-----|--------|---------------------------------------|----------------------------------------|");
 
             for(int i =1; i<result.size();i++){
-                System.out.println(""+i+" "+result.get(i).getName()+" "+result.get(i).getMatric()+" "+result.get(i).getLink());
+                System.out.printf("|%-5s|%-8s|%-39s|%-40s|\n",i+1,result.get(i).getMatric(),result.get(i).getName(),result.get(i).getLink());
+
 
             }
-            System.out.println("-----------------------------------------------------------------------------------------");
+            System.out.println("|-----|--------|---------------------------------------|----------------------------------------|");
 
-        } catch (Exception e) {
-            System.out.println(""+e);
-        }
+
+
+
+    }
+    public ArrayList<Data> getResult() {
         return result;
     }
+
+
 }
