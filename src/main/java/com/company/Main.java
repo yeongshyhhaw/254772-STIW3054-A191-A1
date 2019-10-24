4a91398ce4763348c1d3d0f91a629e85777341ba
yeongshyhhaw <47937370+yeongshyhhaw@users.noreply.github.com>
2019-10-24 19:36:28 +0800

    Update Main.java

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,22 +1,22 @@
 package com.company;
 
+import org.apache.poi.ss.formula.functions.T;
+
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class Main {
-    private static ArrayList<Data> result= new ArrayList<Data>();;
-    private static ArrayList<Data> result2 = new ArrayList<Data>();
+
     public static void main(String[] args) throws IOException {
         Read2 read2 = new Read2();
         Read read = new Read();
-        result2 = read2.ReadData2();
-        result = read.ReadData();
-
+        read.ReadData();
+        read2.ReadData2();
         Compare compare = new Compare();
-        compare.Compare(result,result2);
-
+        compare.check(read.getResult(),read2.getResult2());
+        compare.studentnotsubmitted(read.getResult(),read2.getResult2());
         Excel excel= new Excel();
-        excel.WriteToExcel(result,result2);
+        excel.WriteToExcel(compare.getstudentSub(),compare.getstudentNotSub());
 
     }
 }
